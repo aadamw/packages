eb91db4ad2f3b63a2766530c34194a7868b16ace
Adam Wojtycki <adamwojtycki@gmail.com>
2023-09-23 14:09:02 +0200

    Init prettier package

---
+++ packages/prettier/CHANGELOG.md
@@ -0,0 +1,7 @@
+# @0xaddm/prettier-config
+
+## 1.0.0
+
+### Major Changes
+
+- Initial release
---
+++ packages/prettier/index.mjs
@@ -0,0 +1,28 @@
+/** @typedef  {import("@ianvs/prettier-plugin-sort-imports").PluginConfig} SortImportsConfig*/
+/** @typedef  {import("prettier").Config} PrettierConfig*/
+
+/** @type { PrettierConfig | SortImportsConfig } */
+const config = {
+  arrowParens: 'avoid',
+  bracketSameLine: false,
+  bracketSpacing: true,
+  embeddedLanguageFormatting: 'auto',
+  endOfLine: 'lf',
+  htmlWhitespaceSensitivity: 'css',
+  insertPragma: false,
+  jsxSingleQuote: false,
+  printWidth: 80,
+  proseWrap: 'always',
+  quoteProps: 'as-needed',
+  requirePragma: false,
+  semi: false,
+  singleAttributePerLine: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  plugins: [
+    '@ianvs/prettier-plugin-sort-imports',
+    'prettier-plugin-tailwindcss',
+  ],
+}
+
+export default config
